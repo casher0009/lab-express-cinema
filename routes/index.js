c46d5ad8395725218625f7f4e96ef53fc65424d2
casher0009 <the.casher.uber@gmail.com>
2018-07-08 20:08:34 -0500

    primer commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req,res)=>{
+  Movie.find()
+  .then(movies=>{
+    res.render('movies', {movies})
+    console.log(movies.length)
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+})
+
+router.get('/movies/:id', (req, res)=>{
+
+  let movieId = req.params.id
+  Movie.findOne({'_id' : movieId})
+  .then(movie=>{
+    res.render('movie_detail', {movie})
+    .catch(err=>{
+      console.log(err)
+    })
+  })
+
+})
+
+
+
+
+
 
 module.exports = router;
